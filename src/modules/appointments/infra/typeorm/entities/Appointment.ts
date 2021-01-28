bd8fceb44664d69e303f262466bdb2a9bacb376c
Bruno Duarte <bruno-sacra@hotmail.com>
2021-01-28 18:46:45 -0300

    Implementando estrutura DDD

--- src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -8,26 +8,26 @@ import {
     JoinColumn,
 } from 'typeorm';
 
-import User from './User';
+import User from '@modules/users/infra/typeorm/entities/User';
 
 @Entity('appointments')
 class Appointment {
     @PrimaryGeneratedColumn('uuid')
     id: string;
-
+    
     @Column()
     provider_id: string;
-
+    
     @ManyToOne(() => User)
     @JoinColumn({ name: 'provider_id' })
     provider: User;
-
+    
     @Column('timestamp with time zone')
     date: Date;
-
+    
     @CreateDateColumn()
     created_at: Date;
-
+    
     @UpdateDateColumn()
     updated_at: Date;
 }

b06b326bf9f8abf4518f382e13016c3f6bbe4ae4
Bruno Duarte <bruno-sacra@hotmail.com>
2021-01-30 14:47:08 -0300

    Test appointmentCreate

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -15,7 +16,22 @@ describe('CreateAppointment', () => {
         expect(appointment.provider_id).toBe('416546584351');
     });
 
-    // it('should not be able to create two appointments on the same time', () => {
+    it('should not be able to create two appointments on the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
 
-    // });
+        const appointmentDate = new Date();
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '416546584351',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '416546584351',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
 });

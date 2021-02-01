54fa9f490075134bbdb3426ab1ecbbc383801ca3
Bruno Duarte <bruno-sacra@hotmail.com>
2021-02-01 20:39:46 -0300

    Finalizando testes de reset de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -5,8 +5,6 @@ import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 
-import User from '@modules/users/infra/typeorm/entities/User';
-
 interface IRequest {
     email: string;
 }

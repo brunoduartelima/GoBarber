b7fa2e81a906203d3eb86204a8e18c94e0cee1c9
Bruno Duarte <bruno-sacra@hotmail.com>
2021-02-03 18:44:37 -0300

    Rota e controller de perfil

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -32,6 +32,16 @@ describe('UpdateProfile', () => {
         expect(updatedUser.email).toBe('johntree@mail.com');
     });
 
+    it('should not be able to update the profile from non-existing user', async () => {
+        await expect(
+            updateProfile.execute({
+                user_id: 'non-existing-user-id',
+                name: 'Test',
+                email: 'test@test.com'
+            })
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
     it('should not be able to change to another user email', async () => {
         await fakeUsersRepository.create({
             name: 'John Doe',

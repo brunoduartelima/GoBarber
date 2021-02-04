2d9a819512cb8142a6620c3adf1cec452a964903
Bruno Duarte <bruno-sacra@hotmail.com>
2021-02-04 19:46:09 -0300

    Regras do agendamento

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { getHours, isBefore, startOfHour } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -20,16 +20,24 @@ class CreateAppointmentService {
     ) {}
 
     public async execute({ date, user_id, provider_id }: IRequest): Promise<Appointment> {
-
         const appointmentDate = startOfHour(date);
+
+        if (isBefore(appointmentDate, Date.now()))
+            throw new AppError("You can't create an appointment on a past date.");
+
+        if (user_id === provider_id)
+            throw new AppError("You can't create an appointment with yourself.");
+
+        if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17)
+            throw new AppError('You can only create appointments between 8am and 5pm');
 
         const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
             appointmentDate,
         );
 
-        if (findAppointmentInSameDate) {
+        if (findAppointmentInSameDate)
             throw new AppError('This appointment is already booked');
-        }
+        
 
         const appointment = await this.appointmentsRepository.create({
             provider_id,

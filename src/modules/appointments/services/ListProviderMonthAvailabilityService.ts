d1c6134551a4ca9e5d06723f641a8a12aa7ef698
Bruno Duarte <bruno-sacra@hotmail.com>
2021-02-04 16:02:37 -0300

    Listando dias disponiveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { getDate, getDaysInMonth } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -27,9 +28,25 @@ class ListProviderMonthAvailabilityService {
             month
         });
 
-        console.log(appointments);
+        const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-        return [{ day: 1, available: false }];
+        const eachDayArray = Array.from(
+            { length: numberOfDaysInMonth },
+            (_, index) => index + 1,
+        );
+
+        const availability = eachDayArray.map(day => {
+            const appointmentsInDay = appointments.filter(appointment => {
+                return getDate(appointment.date) === day;
+            });
+
+            return {
+                day,
+                available: appointmentsInDay.length < 10,
+            };
+        });
+
+        return availability;
     }
 }
 

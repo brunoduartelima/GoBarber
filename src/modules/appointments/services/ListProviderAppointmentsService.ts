e8169b960bcf446066428a1779a470f663b6370e
Bruno Duarte <bruno-sacra@hotmail.com>
2021-02-26 19:18:25 -0300

    cache de agendamentos

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -22,18 +22,22 @@ class ListProviderAppointmentsService {
     ) {}
 
     public async execute({ provider_id, day, year, month }: IRequest): Promise<Appointment[]> {
-        const cacheData = await this.cacheProvider.recover('appointments-list');
+        const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-        console.log(cacheData);
+        let appointments = await this.cacheProvider.recover<Appointment[]>(cacheKey);
 
-        const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
-            provider_id,
-            day,
-            month,
-            year, 
-        });
+        if(!appointments) {
+            appointments = await this.appointmentsRepository.findAllInDayFromProvider({
+                provider_id,
+                day,
+                month,
+                year, 
+            });
 
-        await this.cacheProvider.save('appointments-list', 'fa');
+            console.log('Buscou Banco');
+
+            await this.cacheProvider.save(cacheKey, appointments);
+        }
 
         return appointments;
     }
